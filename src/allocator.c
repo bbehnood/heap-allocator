219475f79ad4b5d8a1153d857d7ace21a7fb0ac7
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-02-26 12:26:48 +0330

    Add heap_alloc and heap_free

--- src/allocator.c
+++ src/allocator.c
@@ -1,9 +1,10 @@
+#include "allocator.h"
 #include "internal.h"
 #include <sys/mman.h>
 
 static block_t *heap_head = NULL;
 
-block_t *request_space(size_t size)
+static block_t *request_space(size_t size)
 {
     size_t total_size = size + META_SIZE;
 
@@ -19,7 +20,7 @@ block_t *request_space(size_t size)
     return block;
 }
 
-block_t *find_free_block(size_t size)
+static block_t *find_free_block(size_t size)
 {
     block_t *current = heap_head;
 
@@ -30,4 +31,48 @@ block_t *find_free_block(size_t size)
     }
 
     return NULL;
+}
+
+void *heap_alloc(size_t size)
+{
+    if (size == 0) return NULL;
+
+    block_t *block;
+
+    if (!heap_head)
+    {
+        block = request_space(size);
+        if (!block) return NULL;
+        heap_head = block;
+    }
+    else
+    {
+        block = find_free_block(size);
+
+        if (!block)
+        {
+            block = request_space(size);
+            if (!block) return NULL;
+
+            block_t *last = heap_head;
+
+            while (last->next) last = last->next;
+
+            last->next = block;
+        }
+        else
+        {
+            block->free = 0;
+        }
+    }
+
+    return block + 1;
+}
+
+void heap_free(void *ptr)
+{
+    if (!ptr) return;
+
+    block_t *block = (block_t *)ptr - 1;
+    block->free = 1;
 }
